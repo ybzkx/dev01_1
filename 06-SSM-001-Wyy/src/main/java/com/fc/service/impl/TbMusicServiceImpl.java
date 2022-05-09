e68e1afd610591ae7da20da7c7c3ce7c6386cffe
yaoyao <3348066607@qq.com>
2022-05-09 17:40:23 +0800

    网易云微信小程序

---
+++ 06-SSM-001-Wyy/src/main/java/com/fc/service/impl/TbMusicServiceImpl.java
@@ -0,0 +1,59 @@
+package com.fc.service.impl;
+
+import com.fc.dao.TbMusicMapper;
+import com.fc.entity.TbMusic;
+import com.fc.entity.TbMusicExample;
+import com.fc.service.TbMusicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TbMusicServiceImpl implements TbMusicService {
+    @Autowired
+    private TbMusicMapper musicMapper;
+    @Override
+    public List<TbMusic> findAll() {
+        return musicMapper.selectByExample(null);
+    }
+
+    @Override
+    public TbMusic findById(Integer musicId) {
+        return musicMapper.selectByPrimaryKey(musicId);
+    }
+
+    @Override
+    public TbMusic nextSong(Integer musicId) {
+        int maxId = musicMapper.findMaxId();
+        if (musicId != maxId){
+            musicId++;
+        }else {
+            musicId=1;
+        }
+        return musicMapper.selectByPrimaryKey(musicId);
+
+    }
+
+    @Override
+    public TbMusic prevSong(Integer musicId) {
+        if (musicId != 1){
+            musicId--;
+        }else {
+            musicId=musicMapper.findMaxId();
+        }
+        return musicMapper.selectByPrimaryKey(musicId);
+    }
+
+    @Override
+    public List<TbMusic> search(String keyword) {
+        TbMusicExample tbMusicExample = new TbMusicExample();
+
+        TbMusicExample.Criteria criteria = tbMusicExample.createCriteria();
+
+        criteria.andMusicNameLike("%"+keyword+"%");
+        return musicMapper.selectByExample(tbMusicExample);
+    }
+
+
+}
